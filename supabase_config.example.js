617186c913369537bcd7880e34727fb519859bf9
Bubylda-noob <masik120fila@gmail.com>
2026-09-06 23:06:15 +0300

    Delete supabase_config.example.js

--- supabase_config.example.js
+++
@@ -1,6 +0,0 @@
-// Переименуй этот файл в supabase-config.js и вставь данные из
-// Supabase -> Project Settings -> API.
-window.SUPABASE_CONFIG = {
-  url: "https://hjeotfddvuxiabikugel.supabase.co",
-  anonKey: "sb_publishable_TjcNoge3dhua7objnuXnNg_cVoGbwSK"
-};
